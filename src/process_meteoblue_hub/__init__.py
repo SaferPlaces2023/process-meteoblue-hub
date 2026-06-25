060027e9db4dd4f1c0eac763feef5e1739a6f40e
marco.renzi <marcorenzi8@gmail.com>
2026-06-25 11:06:51 +0200

    fix init export

--- src/process_meteoblue_hub/__init__.py
+++ src/process_meteoblue_hub/__init__.py
@@ -8,3 +8,6 @@ if importlib.util.find_spec('pygeoapi') is not None:
 
 from .main import run_meteoblue_ingestor, run_meteoblue_retriever
 from .utils.strings import parse_event
+
+MeteobluePrecipitationRetrieverProcessor = MeteoblueRetrieverProcessor
+MeteobluePrecipitationIngestorProcessor = MeteoblueIngestorProcessor
